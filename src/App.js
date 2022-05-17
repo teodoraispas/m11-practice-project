059e83ae3073b5c9f61b21897feeef0edb78c523
Teodora Ispas <tispas@EN1411724.local>
2022-05-17 13:39:40 +0300

    Added Modal for cart details

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { Fragment, useState } from "react";
+
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setIsVisible(true);
+  }
+
+  const hideCartHandler = () => {
+    setIsVisible(false);
+  }
+
   return (
-    <div>
-      <Header />
+    <Fragment>
+      {isVisible && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
-    </div>
+    </Fragment>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,12 +4,12 @@ import mealsImage from "../../assets/meals.jpg";
 import styles from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <React.Fragment>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={styles["main-image"]}>
         <img src={mealsImage} alt="A table full of meals" />
